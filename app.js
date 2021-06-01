32879a07f9fa8aaddfe6b18e7a1f278eab55fa9d
Quinton Fults <quinton.fults.work@gmail.com>
2021-06-01 11:36:52 -0700

    code

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
-const adder = (a, b) => {
-  console.log(a + b)
-}
+// const adder = (a, b) => {
+//   console.log(a + b)
+// }
 
 // adder(5, 7)
 
-let a = parseInt(process.argv[2])
-let b = parseInt(process.argv[3])
+// let a = parseInt(process.argv[2])
+// let b = parseInt(process.argv[3])
 
-adder(a, b)
+// adder(a, b)
+
+// let a = parseInt(process.argv[3])
+// let b = parseInt(process.argv[4])
+// let op = process.argv[2]
+
+// switch (op) {
+//   case 'add':
+//     console.log(a + b)
+//     break
+//   case 'subtract':
+//     console.log(a - b)
+//     break
+//   case 'multiply':
+//     console.log(a * b)
+//     break
+//   case 'divide':
+//     console.log(a / b)
+//     break
+// }
+
+const fs = require('fs')
+
+fs.writeFile('some.txt', 'Hello World!', err => {
+  if (err) { console.log(err) }
+  console.log('Success!')
+})
